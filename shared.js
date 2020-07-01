3cbb85fa7dfa00b1de11e97945884ba90217c855
OSCAR VARGAS (CRGL-THIRDPARTY.COM) <oscar_vargas@crgl-thirdparty.com>
2020-07-01 15:36:25 -0600

    changes and theory

--- shared.js
+++ shared.js
@@ -9,16 +9,19 @@ var mobileNav = document.querySelector(".mobile-nav");
 
 // console.dir(backdrop);
 for (var i = 0; i < selectPlanButtons.length; i++) {
-  selectPlanButtons[i].addEventListener("click", function() {
+  selectPlanButtons[i].addEventListener("click", function () {
     // modal.style.display = "block";
     // backdrop.style.display = "block";
     // modal.className = 'open'; // This will actually overwrite the complete class list
     modal.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = 'block';
+    setTimeout(() => {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
-backdrop.addEventListener("click", function() {
+backdrop.addEventListener("click", function () {
   // mobileNav.style.display = 'none';
   mobileNav.classList.remove("open");
   closeModal();
@@ -31,15 +34,23 @@ if (modalNoButton) {
 function closeModal() {
   //   backdrop.style.display = "none";
   //   modal.style.display = "none";
+  console.log('closing');
+  
   if (modal) {
     modal.classList.remove("open");
   }
-  backdrop.classList.remove("open");
+  backdrop.style.display = 'none';
+  setTimeout(() => {
+    backdrop.classList.remove("open");
+  }, 10);
 }
 
-toggleButton.addEventListener("click", function() {
+toggleButton.addEventListener("click", function () {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = 'block';
+  setTimeout(() => {
+    backdrop.classList.add("open");
+  }, 10);
 });
