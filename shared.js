5c0819563b02eb0f453c7b3768de20b7e7f7c116
Oscar Vargas Granados <voscar4955@gmail.com>
2020-07-05 20:42:59 -0600

    finishing section 15

--- shared.js
+++ shared.js
@@ -4,6 +4,7 @@ var modalNoButton = document.querySelector(".modal__action--negative");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 // console.dir(backdrop.style['background-image']);
 
@@ -14,7 +15,7 @@ for (var i = 0; i < selectPlanButtons.length; i++) {
     // backdrop.style.display = "block";
     // modal.className = 'open'; // This will actually overwrite the complete class list
     modal.classList.add("open");
-    backdrop.style.display = 'block';
+    backdrop.style.display = "block";
     setTimeout(() => {
       backdrop.classList.add("open");
     }, 10);
@@ -34,12 +35,12 @@ if (modalNoButton) {
 function closeModal() {
   //   backdrop.style.display = "none";
   //   modal.style.display = "none";
-  console.log('closing');
-  
+  console.log("closing");
+
   if (modal) {
     modal.classList.remove("open");
   }
-  backdrop.style.display = 'none';
+  backdrop.style.display = "none";
   setTimeout(() => {
     backdrop.classList.remove("open");
   }, 10);
@@ -49,8 +50,18 @@ toggleButton.addEventListener("click", function () {
   // mobileNav.style.display = 'block';
   // backdrop.style.display = 'block';
   mobileNav.classList.add("open");
-  backdrop.style.display = 'block';
+  backdrop.style.display = "block";
   setTimeout(() => {
     backdrop.classList.add("open");
   }, 10);
+});
+
+ctaButton.addEventListener("animationstart", (event) => {
+  console.log("Animation started", event);
+});
+ctaButton.addEventListener("animationend", (event) => {
+  console.log("Animation ended", event);
+});
+ctaButton.addEventListener("animationiteration", (event) => {
+  console.log("Animation iterated", event);
 });
